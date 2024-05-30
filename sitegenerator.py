4a830d604fc48b10fdf33ee8842d05de7ff2c241
bito <b.kondrak45@gmail.com>
2024-05-30 16:38:03 -0600

    first commit: created a site generator
    as it is now, just has a for loop running to make 10 sites and adds it to an index page

---
+++ sitegenerator.py
@@ -0,0 +1,41 @@
+import fileinput
+
+
+def create_site(site_name: str) -> None:
+    with open(site_name + ".html", "w") as site:
+        site.write("<!DOCTYPE html>\n")
+        site.write("<html>\n")
+        site.write("<head>\n")
+        site.write(f"<title>{site_name}</title>\n")
+        site.write("</head>\n")
+        site.write(f"<body><p>welcome to {site_name}</p></body>\n")
+        site.write("</html>")
+
+
+def add_to_index(site_name: str) -> None:
+    with open("index.html", "r") as f:
+        data = f.read()
+    some_index_you_want_to_insert_at = data.index("<body>") + len("<body>")
+    some_text_to_insert = f'<a href="{site_name}.html">{site_name}</a>\n'
+
+    new_data = (
+        data[:some_index_you_want_to_insert_at]
+        + some_text_to_insert
+        + data[some_index_you_want_to_insert_at:]
+    )
+
+    with open("index.html", "w") as f:
+        f.write(new_data)
+
+
+def main():
+    # input_site = input()
+    for i in range(10):
+        new_site = "website_" + str(i)
+        create_site(new_site)
+        add_to_index(new_site)
+        print("website number " + str(i) + "created")
+
+
+if __name__ == "__main__":
+    main()
